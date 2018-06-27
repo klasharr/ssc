7508b5e316a4610085f93e81ebf8407583c0d58a
Klaus Harris <klasharr42@gmail.com>
2018-06-27 20:22:20 +0100

    Better openclub-csv dependency handling

---
+++ inc/class-openclub-csv-dependency.php
@@ -0,0 +1,65 @@
+<?php
+
+if( !class_exists( 'Openclub_CSV_Dependency' ) ) {
+
+	class Openclub_CSV_Dependency {
+
+		private function __construct() {
+		}
+
+		public static function check( string $plugin_file ) {
+
+			if ( empty( trim( $plugin_file ) ) ) {
+				error_log( 'Openclub_CSV_Dependency::check() must be passed a string.' );
+				return;
+			}
+
+			if ( ! defined( 'OPENCLUB_CSV_PLUGIN_DIR' ) ) {
+				define( 'OPENCLUB_CSV_PLUGIN_DIR', wp_normalize_path( WP_PLUGIN_DIR ) . '/openclub-csv/' );
+			}
+
+			if ( ! file_exists( OPENCLUB_CSV_PLUGIN_DIR . 'inc/class-factory.php' ) ) {
+				self::log_error();
+				self::deactivate_within_wp_admin( $plugin_file );
+				return;
+			}
+
+			require_once( OPENCLUB_CSV_PLUGIN_DIR . 'inc/class-factory.php' );
+
+			if ( ! defined( 'OPENCLUB_DEFAULT_FILTER_PRIORITY' ) ) {
+				self::log_error();
+				self::deactivate_within_wp_admin( $plugin_file );
+				return;
+			}
+
+			return true;
+		}
+
+		private static function deactivate_within_wp_admin( $plugin_file ) {
+
+			/**
+			 * Check if the openclub-csv plugin is loaded in wp-admin
+			 */
+			add_action( 'admin_init', function () use ( $plugin_file ) {
+
+				deactivate_plugins( plugin_basename( $plugin_file ) );
+				if ( ! empty( $_GET['activate'] ) ) {
+					unset( $_GET['activate'] );
+				}
+				add_action( 'admin_notices', function () use ( $plugin_file ) {
+
+					echo '<div class="notice notice-warning is-dismissible">
+         <p>Plugin OpenClub CSV must exist and be activated. ' . plugin_dir_path( $plugin_file ) . 'has been deactivated</p>
+     </div>';
+				} );
+			} );
+		}
+
+		private static function log_error() {
+
+			error_log( 'The openclub-csv plugin is required. WordPress plugin ssc is exiting early.' );
+		}
+
+	}
+
+}
--- ssc.php
+++ ssc.php
@@ -3,7 +3,7 @@
 /*
  Plugin Name: SSC
  Plugin URI: 
- Description:
+ Description: Collection of functionality for Swanage Sailing Club. Depends on the openclub-csv plugin being active.
  Author: Klaus Harris
  Version: -1
  Author URI: https://klaus.blog
@@ -14,31 +14,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'SSC_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+// ======================== Dependency check =======================
+require_once( 'inc/class-openclub-csv-dependency.php' );
+if(! Openclub_CSV_Dependency::check( __FILE__ ) ){
+	return;
+}
+// =================================================================
 
+define( 'SSC_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 require_once( 'inc/class-ssc-safety-team-filter.php' );
 require_once( 'inc/class-ssc-empty-duties-filter.php' );
 require_once( 'inc/class-ssc-training-filter.php' );
 require_once( 'inc/data.php' );
 require_once( 'inc/shortcodes.php' );
 require_once( 'inc/head.php' );
-
-/**
- * Openclub CSV Dependency Check
- */
-add_action( 'admin_init', function() {
-	if( !class_exists( '\OpenClub\Factory' ) ) {
-		deactivate_plugins( plugin_basename( __FILE__ ) );
-		if( !empty( $_GET['activate'] ) ) {
-			unset($_GET['activate']);
-		}
-		add_action( 'admin_notices', function(){
-			echo '<div class="notice notice-warning is-dismissible">
-             <p>Plugin OpenClub CSV must exist and be activated. ' . plugin_dir_path( __FILE__ ) . 'has been deactivated</p>
-         </div>';
-		});
-	}
-});
 
 if ( class_exists( 'WP_CLI' ) ) {
 
@@ -49,6 +38,10 @@ if ( class_exists( 'WP_CLI' ) ) {
 }
 
 add_filter( 'openclub_csv_display_data', 'ssc_prep_safety_teams_shortcode_data', 10, 2 );
+
+
+
+
 
 
 
